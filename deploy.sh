01d7638659f05fe520eac1e10395746e611274ae
fxzer <wy3010099292@163.com>
2023-11-06 23:48:40 +0800

    ci: 测试 gitee 同步

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,8 @@ git add -A
 
 git commit -m '🎉deploy gh-pages🎉'
 
-git push -f git@gitee.com:fxzer/zerdocs.git master:gh-pages
-# git push -f git@github.com:fxzer/zerdocs.git master:gh-pages
+git push -f git@gitee.com:fxzer/zerdocs.git main:gh-pages
+# git push -f git@github.com:fxzer/zerdocs.git main:gh-pages
 
 cd ..
 
