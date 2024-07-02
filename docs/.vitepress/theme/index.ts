0dc6ac86813fb76c43c1d7c34ef4835f72087eb4
fxzer <fxzer8888@163.com>
2024-07-02 21:38:59 +0800

    fix: 修复切换路由图片预览失效问题

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,32 +1,31 @@
 // import './styles/index.css' // 引入自定义样式:不生效,需要在组件下方引入
 import "./styles/index.css";
 import { h  } from 'vue'
-import { onMounted } from 'vue';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
 import Theme from "vitepress/theme";
 import mediumZoom from 'medium-zoom';
 console.log('[ mediumZoom ]-7', mediumZoom)
 import UnoCSSLayout from './UnoCSSLayout.vue'
 import DemoWrap from "./components/DemoWrap.vue";
-// import Documate from '@documate/vue'
-// import '@documate/vue/dist/style.css'
-// import RegisterSW from './components/RegisterSW.vue'
-// import ReloadPrompt from './components/ReloadPrompt.vue'
 export default {
   ...Theme,
   setup() {
+    const route = useRoute();
+    const initZoom = () => {
+    // @ts-ignore
+      new mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' });
+    };
     onMounted(() => {
-      mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' });
+      initZoom();
     });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
   },
   Layout: () => {
-    return h(UnoCSSLayout, null, {
-      //集成文档 AI 组件
-      // 'nav-bar-content-before': () => h(Documate, {
-      //   endpoint: 'https://vwg4waeeqt.us.aircode.run/ask',
-      // }),
-      //集成 ServiceWorker
-      // 'layout-bottom': () => h(ReloadPrompt)
-    })
+    return h(UnoCSSLayout, null, { })
   },
   enhanceApp(ctx) {
     // 注册全局组件
