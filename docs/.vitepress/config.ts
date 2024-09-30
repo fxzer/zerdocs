62621a7ef48ca7d8647e2ba4e030f136f84f4e7c
fxzer <fxzer8888@163.com>
2024-09-30 17:11:27 +0800

    chore: update deps

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,7 +7,7 @@ import { transformerTwoslash } from '@shikijs/vitepress-twoslash'
 export default defineConfig({
   //根据环境变量决定打包路径
   // base: process.env.NODE_ENV === 'production' ? '/zerdocs/' : '/',
-  // base: '/zerdocs/',//配置打包获取静态资源路径
+  base: '/zerdocs/',//配置打包获取静态资源路径
   outDir: '../dist',
   cleanUrls: true,  //去掉url中的.html后缀
   // srcDir:'./src', //相对于docs
